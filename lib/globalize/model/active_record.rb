0be5d98893f2c05b5a4820b8bd60ccb774344c4b
Patrick Huesler <patrick.huesler@gmail.com>
2009-10-04 15:24:48 +0200

    Check for the existence of the proxy class because it might exist already.  Due to rails lazy class loading using const_missing I try to load the proxy class,  if that fails I create a new one. After that, append the globalize methods to the  class.
     I did not find a way to reproduce this error with a unit test. So here is how
     you can reproduce this:
    
     1. Have a model that needs translation (e.g Tag)
     2. Create a TagTranslation model and add a custom method to it (e.g slugname)
     3. ruby script/console
     4. Tag.new (Tag class is loaded and proxy class is being created)
     5. TagTranslation.new.respond_to?(:slugname) => false but should be true

--- lib/globalize/model/active_record.rb
+++ lib/globalize/model/active_record.rb
@@ -11,18 +11,29 @@ module Globalize
           module_names = klass.name.split('::')
           klass_name   = module_names.pop
           target       = module_names.empty? ? Object : module_names.join('::').constantize
+          proxy_class_name = "#{klass_name}Translation"
+          proxy_class = nil
+          begin
+            proxy_class = proxy_class_name.constantize
+          rescue NameError
+            proxy_class = target.const_set proxy_class_name, Class.new(::ActiveRecord::Base)
+          end
 
-          target.const_set "#{klass_name}Translation", Class.new(::ActiveRecord::Base) {
-            belongs_to "#{klass.name.underscore.gsub('/', '_')}".intern
+          proxy_class.instance_eval do
+              belongs_to "#{klass.name.underscore.gsub('/', '_')}".intern
+           end
 
-            def locale
-              read_attribute(:locale).to_sym
-            end
+           proxy_class.class_eval do
+             def locale
+               read_attribute(:locale).to_sym
+             end
 
-            def locale=(locale)
-              write_attribute(:locale, locale.to_s)
-            end
-          }
+             def locale=(locale)
+               write_attribute(:locale, locale.to_s)
+             end
+          end
+
+           return proxy_class
         end
 
         def define_accessors(klass, attr_names)
